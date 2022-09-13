45e26d3af4bd9a9e6044a4a96ce94e3a042d768c
Juan Covarrubias <me@juan.fi>
2022-09-13 09:03:51 +0300

    added autopep8 to project

--- src/app.py
+++ src/app.py
@@ -3,9 +3,11 @@ from flask import render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
     return render_template("index.html")
+
 
 @app.route("/test")
 def test_page():
--- src/tests/test_app.py
+++ src/tests/test_app.py
@@ -2,5 +2,5 @@ def test_index(app, client):
     expected = "Hello world!"
 
     result = client.get("/")
-    
+
     assert expected in result.text
