46989a28dc7d71d40ca6e22dddb973bf67c0ea76
Juan Covarrubias <me@juan.fi>
2022-09-13 09:35:33 +0300

    Added invoke to project, created invoke tasks and updated README

---
+++ tasks.py
@@ -0,0 +1,17 @@
+from invoke import task
+
+@task
+def start(ctx):
+    ctx.run("cd src; flask run")
+
+@task
+def lint(ctx):
+    ctx.run("pylint src")
+
+@task
+def tests(ctx):
+    ctx.run("pytest")
+
+@task
+def e2etests(ctx):
+    ctx.run("robot src/tests/robot_tests/")
