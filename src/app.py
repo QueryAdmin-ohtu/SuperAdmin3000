c459c545ae8c493c8fa9a307a7d0a97f5233cfba
ramipiik <rami.piik@gmail.com>
2022-09-17 19:31:13 +0300

    lint

--- src/app.py
+++ src/app.py
@@ -1,18 +1,20 @@
+from os import getenv
+import secrets
 from flask import Flask
 from flask import render_template, redirect, session, request, abort
 from google.oauth2 import id_token
 from google.auth.transport import requests
 # from flask_cors import CORS
-from os import getenv
-import secrets
 
 app = Flask(__name__)
 # CORS(app)
 
 app.secret_key = "ffobar" #getenv("SECRET_KEY")
 CLIENT_ID="210669772745-35ee1vb4rv29ss1v6c6ea9gd57ies2oi.apps.googleusercontent.com"  #Google cloud id
-#TODO: Create a proper storage for the authorized users
-authorized_users=["antti.vainikka36@gmail.com", "jatufin@gmail.com","me@juan.fi", "niemi.leo@gmail.com", "oskar.sjolund93@gmail.com", "rami.piik@gmail.com", "siljaorvokki@gmail.com"]
+
+# TODO: Create a proper storage for the authorized users
+authorized_google_accounts=["antti.vainikka36@gmail.com", "jatufin@gmail.com","me@juan.fi",
+"niemi.leo@gmail.com", "oskar.sjolund93@gmail.com", "rami.piik@gmail.com", "siljaorvokki@gmail.com"]
 
 @app.route("/", methods=["GET"])
 def index():
@@ -23,13 +25,14 @@ def index():
     """
     if _logged_in():
         return render_template("index.html")
-    else:
-        return render_template("google_login.html")
+    return render_template("google_login.html")
 
 @app.route("/google_login", methods=["POST"])
 def google_login():
+    """ Login with a Google account.
+    """
     try:
-        csrf_token_cookie = request.cookies.get('g_csrf_token')      
+        csrf_token_cookie = request.cookies.get('g_csrf_token')
         if not csrf_token_cookie:
             abort(400, 'No CSRF token in Cookie.')
         csrf_token_body = request.form['g_csrf_token']
@@ -37,7 +40,6 @@ def google_login():
             abort(400, 'No CSRF token in post body.')
         if csrf_token_cookie != csrf_token_body:
             abort(400, 'Failed to verify double submit cookie.')
-        
         token=request.form["credential"]
         if not token:
             abort(400, 'No token found.')
@@ -50,7 +52,6 @@ def google_login():
     except ValueError:
         # Invalid token
         pass
-    
     if _google_login_authorize(email):
         session["email"] = email
         session["username"]=first_name
@@ -59,16 +60,16 @@ def google_login():
     return "You are not authorized to use the service. Please contact your administrator."
 
 def _google_login_authorize(email):
-    """ Checks whether an authenticated Google account is authorized to access the service.
+    """ Checks whether a Google account is authorized to access the app.
     """
-    if email in authorized_users:
+    if email in authorized_google_accounts:
         return True
     return False
 
 def _logged_in():
     """ Check if the session is active. This should be allways used before
     rendering pages.
-    """ 
+    """
     return "username" in session
 
 def valid_token(form):
@@ -113,23 +114,22 @@ def _clear_session():
     """ Logout the user and clear session properties
     """
     _remove_from_session("csrf_token")
-    _remove_from_session("username")    
+    _remove_from_session("username")
 
-def _remove_from_session(property):
+def _remove_from_session(property_key):
     """ Checks if the given property name can be found in the session
     and removes it
     """
-    if property in session:
-        del session[property]
+    if property_key in session:
+        del session[property_key]
 
 @app.route("/backdoor", methods=["GET"])
 def backdoor_form():
-    """ Form for logging in without Google signin
+    """ Form for logging in without Google
     """
     if _logged_in():
         return render_template("index.html")
-    else:
-        return render_template("backdoor_login.html")
+    return render_template("backdoor_login.html")
 
 @app.route("/backdoor", methods=["POST"])
 def backdoor_login():
@@ -140,14 +140,12 @@ def backdoor_login():
 
     if not _backdoor_validate_and_login(username, password):
         return abort(401)
-        
     return redirect("/")
 
 def _backdoor_validate_and_login(username, password):
     """ Check if the given username password pair is correct
-    If username and password match, a new session will be created 
+    If username and password match, a new session will be created
     """
-
     # TODO: Proper data storage for usernames and password hashes
     if username != "rudolf":
         return False
@@ -156,5 +154,4 @@ def _backdoor_validate_and_login(username, password):
 
     session["csrf_token"] = secrets.token_hex(16)
     session["username"] = username
-    
     return True
