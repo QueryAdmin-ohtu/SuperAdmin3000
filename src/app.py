a65231ce9b287e5ad900c54ca5e64e887230e5bc
Silja <44512829+Ronttikasa@users.noreply.github.com>
2022-09-27 19:01:43 +0300

    Move imports

--- src/app.py
+++ src/app.py
@@ -12,24 +12,6 @@ app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URI")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
-if ENV == 'local':
-    print("ENV: local")
-    GOOGLE_URI = "http://localhost:5000/google_login"
-elif ENV == 'test':
-    print("ENV: test")
-    GOOGLE_URI = "TO BE DEFINED"
-elif ENV == 'prod':
-    print("ENV: prod")
-    GOOGLE_URI = "https://superadmin3000.herokuapp.com/google_login"
-else:
-    print("ENV:", ENV)  # Should this throw an error?
-
-# TODO: Create a proper storage for the authorized users
-# pylint: disable-next=line-too-long
-authorized_google_accounts = ["antti.vainikka36@gmail.com", "jatufin@gmail.com", "me@juan.fi",
-# pylint: disable-next=line-too-long
-                              "niemi.leo@gmail.com", "oskar.sjolund93@gmail.com", "rami.piik@gmail.com", "siljaorvokki@gmail.com"]
-
 from views import *
 
 if __name__ == "__main__":
--- src/views.py
+++ src/views.py
@@ -2,7 +2,25 @@ from flask import render_template, redirect, session, request, abort
 import secrets
 from google.oauth2 import id_token
 from google.auth.transport import requests
-from app import app, CLIENT_ID, ENV, db, GOOGLE_URI, authorized_google_accounts
+from app import app, CLIENT_ID, ENV, db
+
+if ENV == 'local':
+    print("ENV: local")
+    GOOGLE_URI = "http://localhost:5000/google_login"
+elif ENV == 'test':
+    print("ENV: test")
+    GOOGLE_URI = "TO BE DEFINED"
+elif ENV == 'prod':
+    print("ENV: prod")
+    GOOGLE_URI = "https://superadmin3000.herokuapp.com/google_login"
+else:
+    print("ENV:", ENV)  # Should this throw an error?
+
+# TODO: Create a proper storage for the authorized users
+# pylint: disable-next=line-too-long
+authorized_google_accounts = ["antti.vainikka36@gmail.com", "jatufin@gmail.com", "me@juan.fi",
+# pylint: disable-next=line-too-long
+                              "niemi.leo@gmail.com", "oskar.sjolund93@gmail.com", "rami.piik@gmail.com", "siljaorvokki@gmail.com"]
 
 @app.route("/testdb/new")
 def new_message():
