a8f17e569cf6779635fc1f8f085804458311fdd9
ramipiik <rami.piik@gmail.com>
2022-09-29 23:28:11 +0300

    delete unnecessary file

--- init_local_db.sh
+++
@@ -1,3 +0,0 @@
-ls
-psql localdb
-drop table if exists "Admins", "Categories", "Category_results", "Industries", "Organizations", "Question_answers", "Questions", "Survey_results", "Survey_user_groups", "Surveys", "User_answers", "Users" cascade;
