d1be58c6c5516fdffbdf64aebff6c3ce90523784
Janne Tuukkanen <jatufin@gmail.com>
2022-10-02 22:45:21 +0300

    Move importing of views.py after app initialization

--- src/app.py
+++ src/app.py
@@ -1,7 +1,5 @@
-from views import *  # pylint: disable=unused-wildcard-import, wrong-import-order, wildcard-import, cyclic-import
-
-from flask import Flask  # pylint: disable=wrong-import-order
-from flask_sqlalchemy import SQLAlchemy  # pylint: disable=wrong-import-order
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from config import PORT, load_config
 
 # Create Flask app
@@ -11,6 +9,8 @@ app.config.from_object(configs)
 
 db = SQLAlchemy(app)
 
+# This is here, because the views.py imports from app
+from views import *  # pylint: disable=unused-wildcard-import, wrong-import-order, wrong-import-position, wildcard-import, cyclic-import
 
 if __name__ == "__main__":
     app.run(debug=False, host="0.0.0.0", port=PORT)
