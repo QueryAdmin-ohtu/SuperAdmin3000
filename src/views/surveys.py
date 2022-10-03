24b43abcf84c8c5140e1450c402002b7adbe1b09
Janne Tuukkanen <jatufin@gmail.com>
2022-10-04 02:03:44 +0300

    Add survey edit form

--- src/views/surveys.py
+++ src/views/surveys.py
@@ -17,7 +17,7 @@ def new():
 
 
 @surveys.route("/surveys/edit/<survey_id>")
-def edit(survey_id):
+def surveys_edit(survey_id):
     """Renders the edit survey page
     """
     if not helper.logged_in():
@@ -27,6 +27,26 @@ def edit(survey_id):
     
     return render_template("surveys/edit.html", survey=survey, survey_id=survey_id)
 
+@surveys.route("/surveys/update", methods=["POST"])
+def surveys_update():
+    """ Form handler for updating an existing survey info
+    """
+
+    if not helper.valid_token(request.form):
+        abort(403)
+
+    survey_id = request.form["survey_id"]
+    name = request.form["name"]
+    title = request.form["title"]
+    survey = request.form["text"]
+
+    # TODO: Implement the SQL query for updating a survey
+    #  result = queries.update_survey(survey_id, name, title, text)
+
+    route = f"/surveys/{survey_id}"
+
+    return redirect(route)
+    
 @surveys.route("/create_survey", methods=["POST"])
 def create_survey():
     """ Takes arguments from new.html
@@ -41,7 +61,7 @@ def create_survey():
     title = request.form["title"]
     survey = request.form["survey"]
     survey_id = queries.create_survey(name, title, survey)
-    route = "/surveys/" + str(survey_id)
+    route = f"/surveys/{survey_id}"
 
     return redirect(route)
 
@@ -58,3 +78,19 @@ def view_survey(survey_id):
     survey = queries.get_survey(survey_id)
 
     return render_template("surveys/view_survey.html", survey=survey, survey_id=survey_id)
+
+
+@surveys.route("/surveys/statistics/<survey_id>")
+def surveys_statistics(survey_id):
+    """ Open up statistics for the given survey
+    """
+
+    if not helper.logged_in():
+        return redirect("/")
+
+    survey = queries.get_survey(survey_id)
+    
+    #  TODO: get statistics
+    statistics = "JUGE STATS HERE!"
+
+    return render_template("surveys/statistics.html", survey=survey, statistics=statistics, survey_id=survey_id)
