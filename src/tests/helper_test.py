b22fe781a196ceee524e18d4d08d2220dbbeb4ab
Janne Tuukkanen <jatufin@gmail.com>
2022-10-06 17:26:01 +0300

    Create unit tests for helper methods

---
+++ src/tests/helper_test.py
@@ -0,0 +1,72 @@
+import pytest
+
+from app import create_app
+import helper
+
+@pytest.fixture()
+def app():
+    app = create_app()
+
+    yield app
+
+@pytest.fixture()
+def client(app):
+    return app.test_client()
+
+@pytest.fixture()
+def runner(app):
+    return app.test_cli_runner()
+
+def test_request_example(client):
+    response = client.get("/")
+    assert b"Super" in response.data
+
+def test_backdoor_validate_and_login_with_valid_credentials(app):
+    with app.test_request_context() as context:
+        response = helper.backdoor_validate_and_login("rudolf", "secret")
+
+        assert response == True
+        assert context.session["username"] == "rudolf"
+        assert context.session["csrf_token"]
+
+def test_backdoor_validate_and_login_with_invalid_credentials(app):
+    with app.test_request_context() as context:
+        response = helper.backdoor_validate_and_login("rudolf", "wrong")
+
+        assert response == False
+        assert "username" not in context.session
+        assert "csrf_token" not in context.session
+    
+        
+def test_update_session(app):
+    with app.test_request_context() as context:
+        helper.update_session("user@mail", "user", "token")
+
+        assert context.session["email"] == "user@mail"
+        assert context.session["username"] == "user"
+        assert context.session["csrf_token"] == "token"        
+
+def test_clear_session(app):
+    with app.test_request_context() as context:
+        context.session["email"] = "user@mail"
+        context.session["username"] = "user"
+        context.session["csrf_token"] = "token"        
+
+        helper.clear_session()
+        assert "email" not in context.session        
+        assert "username" not in context.session
+        assert "csrf_token" not in context.session
+
+def test_logged_in_when_logged_in(app):
+    with app.test_request_context() as context:
+        context.session["username"] = "user"
+
+        response = helper.logged_in()
+        
+        assert response
+
+def test_logged_in_when_not_logged_in(app):
+    with app.test_request_context() as context:
+        response = helper.logged_in()
+        
+        assert not response
