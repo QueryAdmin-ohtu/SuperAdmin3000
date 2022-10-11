e000e05f86d0a6f3ae5e9b78b4712f5fc4b4738e
Vainikka Antti I <anttvain@cubbli22-gold.local>
2022-10-11 20:37:33 +0300

    Fixed lint

--- src/views/surveys.py
+++ src/views/surveys.py
@@ -96,7 +96,8 @@ def view_survey(survey_id):
     survey = survey_service.get_survey(survey_id)
     questions = survey_service.get_questions_of_survey(survey_id)
 
-    return render_template("surveys/view_survey.html", survey=survey, questions=questions, survey_id=survey_id, ENV=app.config["ENV"])
+    return render_template("surveys/view_survey.html", survey=survey,
+    questions=questions, survey_id=survey_id, ENV=app.config["ENV"])
 
 
 @surveys.route("/surveys/statistics/<survey_id>")
@@ -112,7 +113,8 @@ def surveys_statistics(survey_id):
     #  TODO: get statistics
     statistics = "JUGE STATS HERE!"
 
-    return render_template("surveys/statistics.html", survey=survey, statistics=statistics, survey_id=survey_id, ENV=app.config["ENV"])
+    return render_template("surveys/statistics.html", survey=survey,
+    statistics=statistics, survey_id=survey_id, ENV=app.config["ENV"])
 
 
 @surveys.route("/add_question", methods=["POST"])
