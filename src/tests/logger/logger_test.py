aca350fbcc18361b5cfc56b3922f1c750955aa42
Janne Tuukkanen <jatufin@gmail.com>
2022-10-15 13:16:11 +0300

    Unittests for logger

--- src/tests/logger/logger_test.py
+++ src/tests/logger/logger_test.py
@@ -7,23 +7,26 @@ from logger.logger import Logger
 class TestLogger(unittest.TestCase):
     def setUp(self):
         self.filename = "logfile.log"
-        self.logger = Logger(self.filename)
+        self.logger = Logger("testuser", self.filename)
 
-    @patch("builtins.open", mock_open(read_data="""[FOO] 31-12-2099 15:55:55 This is an example event 
-[BAR] 02-02-2100 11:11:11 This is a second example event"""))
+    @patch("builtins.open", mock_open(read_data="""[EVENT] 31-12-2099 15:55:55 This is an example event 
+[ERROR] 02-02-2100 11:11:11 This is a second example event"""))
     def test_get_all_events_returns_text(self):
         result = self.logger.read_all_events()
 
         self.assertEqual(len(result), 2)
         self.assertTrue("example" in result[0])
-        self.assertTrue("BAR" in result[1])
+        self.assertTrue("ERROR" in result[1])
         
         open.assert_called_with(self.filename, "r")
         
     @patch("builtins.open")
     def test_log_event_adds_log_entry(self, m):
-        result = self.logger.write("testuser", "This is a test02")
+        result = self.logger.write("This is a test02")
 
-        self.assertTrue(result)
-        open.write.assert_called_with("foo", "x")
+        self.assertTrue("test02" in result)
+
+        open.assert_called_once_with(self.filename, "a")
+        # TODO: fix this
+        # open.write.assert_called_once_with(result)
         
