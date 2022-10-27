53415e68a88604c59fd18b45ddcb97ef1b0d1913
Janne Tuukkanen <jatufin@gmail.com>
2022-10-27 03:08:41 +0300

    Add ability to change the sort order of the event log

--- src/views/logs.py
+++ src/views/logs.py
@@ -14,5 +14,16 @@ def logs():
         return redirect("/")
 
     logs = Logger().read_all_events()
+    logs.reverse()
+    
+    return render_template("logs/logs.html", logs=logs, reverse=False)
 
-    return render_template("logs/logs.html", logs=logs)
+@log.route("/logs/oldestfirst")
+def logs_reversed():
+    if not helper.logged_in():
+        flash("Log in to use the application", "error")
+        return redirect("/")
+
+    logs = Logger().read_all_events()
+    
+    return render_template("logs/logs.html", logs=logs, reverse=True)
