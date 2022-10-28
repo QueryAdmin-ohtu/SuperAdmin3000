6eff5d390b2df0fae38177b4fb7379f14e0ea3c8
Oskar <oskar.sjolund93@gmail.com>
2022-10-28 19:35:49 +0300

    submission and distribution functions for survey stats

--- src/services/survey_service.py
+++ src/services/survey_service.py
@@ -334,6 +334,28 @@ class SurveyService:
         """
 
         return self.survey_repository.delete_category(category_id)
+    
+    def _get_number_of_submissions(self, survey_id):
+        """
+        Fetches the number of submissions for the given
+        survey
+        
+        Returns:
+        Number of submissions as integer
+        """
+
+        return self.survey_repository.get_number_of_submissions(survey_id)
+    
+    def _get_answer_distribution(self, survey_id):
+        """
+        Fetches the distribution of user answers over the
+        answer options of a survey
+        
+        Returns a table with question id and text, answer
+        id and text, number of user answers
+        """
+
+        return self.survey_repository.get_answer_distribution(survey_id)
 
 
 survey_service = SurveyService(SurveyRepository())
