f31acc2ce9c6fdd6b66bb7a48e5a5d882ca1bd3c
Oskar <oskar.sjolund93@gmail.com>
2022-11-03 20:40:28 +0200

    answer charts passed to template

--- src/views/statistics.py
+++ src/views/statistics.py
@@ -9,17 +9,18 @@ stats = Blueprint("stats", __name__)
 @stats.route("/surveys/<survey_id>/statistics", methods=["GET"])
 def statistics(survey_id):
     """Shows the statistics for the specific survey"""
-    return redirect("/")
-    
+
     if not helper.logged_in():
         flash("Log in to use the application", "error")
         return redirect("/")
-    
-    submissions = survey_service.get_number_of_submissions_for_survey(survey_id)
-    answer_distribution = survey_service.get_answer_distribution_for_survey_questions(survey_id)
 
+    submissions = survey_service.get_number_of_submissions_for_survey(survey_id)
+    q_names, q_ids = helper.save_question_answer_charts(
+        survey_service.get_answer_distribution_for_survey_questions(survey_id)
+    )
     return render_template("surveys/statistics.html",
                            ENV=app.config["ENV"],
                            survey_id=survey_id,
                            submissions=submissions,
-                           answer_distribution=answer_distribution)
+                           q_names=q_names,
+                           q_ids=q_ids)
