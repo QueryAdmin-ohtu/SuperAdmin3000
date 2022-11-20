353d68f80dc5babce6cdcb45636736889336717a
Leo Niemi <niemi.leo@gmail.com>
2022-11-20 12:48:41 +0200

    Bugfix

--- src/views/surveys.py
+++ src/views/surveys.py
@@ -398,7 +398,7 @@ def add_category_result(survey_id, category_id):
             return redirect(f"/edit_category/{survey_id}/{category_id}/new-category-result")
         if original_results != new_results:
             survey_service.update_category_results(
-                original_results, new_results, survey_id, category_id)            
+                original_results, new_results, survey_id)
     if new_cat_result_text and new_cat_cutoff:
         survey_service.create_category_result(category_id, new_cat_result_text, new_cat_cutoff)
     return redirect(f"/edit_category/{survey_id}/{category_id}/new-category-result")
